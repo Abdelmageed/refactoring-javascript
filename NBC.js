16bafe48f3e66335c37aef0d816bc1345328c5cf
Abdelmageed <abdelmageedsayed@hotmail.com>
2017-10-02 21:26:44 +0200

    Removed dead code.

--- NBC.js
+++ NBC.js
@@ -1,18 +1,14 @@
-fs = require('fs');
-
 imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
 someWhereOverTheRainbow = ['c', 'em', 'f', 'g', 'am'];
 tooManyCooks = ['c', 'g', 'f'];
 iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
 babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
 creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
-army = ['ab', 'ebm7', 'dbadd9', 'fm7', 'bbm', 'abmaj7', 'ebm'];
 paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7',
 'em7', 'a7', 'f7', 'b'];
 toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
 'g7'];
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
-blankSong = [];
 var songs = [];
 var labels = [];
 var allChords = [];
@@ -29,7 +25,7 @@ function train(chords, label) {
             allChords.push(chords[index]);
         }
     }
-    if(!!(Object.keys(labelCounts).includes(label))) {
+    if((Object.keys(labelCounts).includes(label))) {
         labelCounts[label] = labelCounts[label] + 1;
     } else {
         labelCounts[label] = 1;
@@ -66,7 +62,7 @@ function setProbabilityOfChordsInLabels() {
     probabilityOfChordsInLabels = chordCountsInLabels;
     Object.keys(probabilityOfChordsInLabels).forEach(difficulty => {
         Object.keys(probabilityOfChordsInLabels[difficulty]).forEach(chord => {
-            probabilityOfChordsInLabels[difficulty][chord] = probabilityOfChordsInLabels[difficulty][chord] * 1.0 * songs.length;
+            probabilityOfChordsInLabels[difficulty][chord] = probabilityOfChordsInLabels[difficulty][chord] * songs.length;
         });
     });
 }
@@ -85,16 +81,12 @@ setChordCountsInLabels();
 setProbabilityOfChordsInLabels();
 
 function classify(chords) {
-    var total = labelProbabilities;
-    console.log(total);
     var classified = {};
-    Object.keys(total).forEach(difficulty => {
+    Object.keys(labelProbabilities).forEach(difficulty => {
         var first = labelProbabilities[difficulty] + 1.01;
         chords.forEach(chord => {
             var probabilityOfChordInLabel = probabilityOfChordsInLabels[difficulty][chord];
-            if (probabilityOfChordInLabel === undefined) {
-                first + 1.01;
-            } else {
+            if (probabilityOfChordInLabel) {
                 first = first * (probabilityOfChordInLabel * 1.01);
             }
         });
